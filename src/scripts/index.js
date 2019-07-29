0e73904897a222090921620d6fa1af0311c9e3dd
arleonsa <andrei.r.leon.salas@intel.com>
2019-07-29 15:09:55 -0600

    module 3

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,14 @@
 console.log('webpack starterkit');
+const carID = 100;
+
+function sendcars(...allcarsIds){
+allcarsIds.forEach( id => console.log(id));
+
+}
+sendcars(100,200,555);
+
+let car = {id:4000, style: 'bocho'};
+let id,style; 
+({id,style} = car);
+
+console.log(id,style);
